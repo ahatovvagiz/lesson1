6f126b7c856a896e0dae5073be2ed3842a3d5fc3
vagiz <ahatovvagiz@gmail.com>
2024-04-12 21:06:46 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿//Написать программу-калькулятор, вычисляющую выражения вида a + b, a - b, a / b, a * b, введенные из командной строки, и выводящую результат выполнения на экран.
+using System;
+
+namespace lesson1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            bool error = false;
+            if (int.TryParse(args[0], out int a) == false)
+            {
+                Console.WriteLine("Неверно задано значение A");
+                error = true;
+            }
+            if (int.TryParse(args[2], out int b) == false)
+            {
+                Console.WriteLine("Неверно задано значение B");
+                error = true;
+            }
+            if (error)
+            {
+                return;
+            }
+            switch (args[1])
+            {
+                case "+":
+                    Console.WriteLine(a + b);
+                    break;
+                case "-":
+                    Console.WriteLine(a - b);
+                    break;
+                case "/":
+                    Console.WriteLine(a / b);
+                    break;
+                case "*":
+                    Console.WriteLine(a * b);
+                    break;
+                default:
+                    Console.WriteLine("Неизвестная арифметическая операция");
+                    error = true;
+                    break;
+            }
+        }
+    }
+}
